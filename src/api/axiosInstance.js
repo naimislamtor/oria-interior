8fcf52105bb2fa9dbf24c495d81bf5ac2dda4107
naimislamtor <naimislamtor@gmail.com>
2026-08-05 22:11:44 +0600

    update pages and api

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5000",
+  baseURL: "https://oira-interior-server.onrender.com",
   withCredentials: true,
 });
 
